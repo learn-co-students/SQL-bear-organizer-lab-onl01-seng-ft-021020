ccb798ffe50807f83a6d0921a834cba9fd690fa2
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-10 00:22:38 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "male", "black", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Rowdy", 2, "female", "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Tabitha", 3, "male", "red", "aloof", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Sergant Brown", 4, "female", "yellow", "timid", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Melissa", 5, "male", "purple", "adamant", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Grinch", 6, "female", "green", "hardy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+"Wendy", 7, "male", "gold", "modest", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+NULL, 8, "female", "platinum", "brave", 0);
